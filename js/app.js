bb3feb3254867dde175bec961703fd1460b5d1a4
margb13 <marg.b13@hotmail.com>
2017-11-03 11:43:16 -0500

    Corrigiendo con ES Lint

--- js/app.js
+++ js/app.js
@@ -1,48 +1,58 @@
- 
-
 var resultado = document.getElementById("resultado"); // variable resultado es el lugar donde mi elemento que se encuentra en mi html donde me mostrara el resultado el valor unicode 
 
 var cipher = function(){
     var cadena = document.getElementById("entrada").value; //Obtener el valor de lo que ingresa el usuario input type=text
-    console.log(cadena);
-	if(cadena.length == 0) {
-		var alerta = alert('Error. el campo esta vacio');
+    
+    var alerta = "";
+	if(cadena.length === 0) {
+		alerta = alert('Error. el campo esta vacio');
 		return alerta;
 	}else if(!isNaN(cadena)){
-		var alerta = alert('Error. el campo solo acepta texto');
+		alerta = alert('Error. el campo solo acepta texto');
 		return alerta; 
-	};
+	}
+    
+    console.log(cipher);
    
     var caracteres = []; //creamos un array 
     var caracteresCipher =[];
       
  
-  	    for ( i = 0; i < cadena.length; i++){ //Recorrer la cadena que es el texto ingresado por el usuario
+  	    for ( var i = 0; i < cadena.length; i++){ //Recorrer la cadena que es el texto ingresado por el usuario
             caracteres[i] = cadena.charAt(i).charCodeAt(0); // El array caracteres almacenara los caracteres especificado de una cadena 
             //junto con el método charCodeAt la cual devuelve un número indicando el valor Unicode del carácter en el índice proporcionado.
         }
-        for ( n = 0; n < caracteres.length; n++){
+        for ( var n = 0; n < caracteres.length; n++){
             
-            var cipherNumber = (caracteres[n]  - 65 + 33) % 26 + 65
+            var cipherNumber = (caracteres[n]  - 65 + 33) % 26 + 65;
             caracteresCipher.push(String.fromCharCode(cipherNumber));//// El array caracteres almacenara los caracteres con el método devuelve una cadena creada mediante el uso de una secuencia de valores Unicode especificada.
            
 
            console.log(cipherNumber);
         }
 
-             
-    resultado.innerHTML = caracteresCipher.toString();//esta propiedad devuelve la sintaxis HTML (resultado) de mi array caracteres. 
+        console.log(caracteresCipher.join(""));     
+    resultado.innerHTML = caracteresCipher.join("");//esta propiedad devuelve la sintaxis HTML (resultado) de mi array caracteres.
+    
 };
 
 var decipher = function(){
     var cadena = document.getElementById("entrada").value; //Obtener el valor de lo que ingresa el usuario input type=text
     console.log(cadena);
+    var alerta = "";
+	if(cadena.length === 0) {
+		alerta = alert('Error. el campo esta vacio');
+		return alerta;
+	}else if(!isNaN(cadena)){
+		alerta = alert('Error. el campo solo acepta texto');
+		return alerta; 
+	}
     
     var caracteres = cadena.split(','); //dividira cadena con una separacion de coma la cual resultara mi variable caracteres
  
-  	    for ( i = 0; i < cadena; i++){
+  	    for ( var i = 0; i < cadena; i++){
     	    caracteres[i] = String.fromCharCode(caracteres[i]);//// El array caracteres almacenara los caracteres con el método devuelve una cadena creada mediante el uso de una secuencia de valores Unicode especificada.
   	    }
     resultado.innerHTML = caracteres.toString();//esta propiedad devuelve la sintaxis HTML (resultado) de mi array caracteres. 
 };
-
+console.log(decipher);
