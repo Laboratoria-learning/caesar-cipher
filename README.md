0188d8aad17c7f2407f457c3c3eec5758cf0746f
Gabrielagm <gabrielagomezmilla@gmail.com>
2017-11-05 21:32:15 -0500

    añade archivo .eslintrc y se hacen correcciones al código

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Cifrado de César
+
 ## Primer ejercicio Producto Final
+
 Consideraciones:
-1. Tu programa debe ser capaz de cifrar y descifrar tanto letras mayúsculas como minúsculas. La fórmula para descifrar es: (x - n) % 26
-2. El código debe estar compuesto por 2 funciones con los siguientes nombres: cipher y decipher
-3. El usuario no debe poder ingresar un campo vacío o que contenga números.
+1.Tu programa debe ser capaz de cifrar y descifrar tanto letras mayúsculas como minúsculas. La fórmula para descifrar es: (x - n) % 26
+
+2.El código debe estar compuesto por 2 funciones con los siguientes nombres: cipher y decipher
+
+3.El usuario no debe poder ingresar un campo vacío o que contenga números.
 
 ### Diagrama de flujo
 
@@ -12,21 +16,22 @@ Consideraciones:
 ### Pseudocódigo
 
 ~~~~
+
 usuario ingresa datos
 lee datos
 procedimiento cipher
-     para (var i=0; i<word.length;i++)
-	   Si datos es igual a string y no está vacío
-		convertir a mayúsculas
-		ubicar valor ASCCI
-		Aplicar cifrado Cesar
-		Almacenar en array
-    	   fin para
-    procedimiento decipher
-     para (var i =0; i<word2.length;i++)
-		ubicar valor ASCCI
-		Aplicar cifrado Cesar
-		Almacenar en array
-    	   fin para
+para (var i=0; i<word.length;i++)
+Si datos es igual a string y no está vacío
+convertir a mayúsculas
+ubicar valor ASCCI
+Aplicar cifrado Cesar
+Almacenar en array
+fin para
+procedimiento decipher
+para (var i =0; i<word2.length;i++)
+ubicar valor ASCCI
+Aplicar cifrado Cesar
+Almacenar en array
+fin para
 
 ~~~~
--- js/app.js
+++ js/app.js
@@ -1,40 +1,39 @@
-function cipher(word){
+function cipher(word) {
 
   var newString;
   var newPositionEncript;
   var positionAlphabetAscii;
-  var encripWord=[];
+  var encripWord = [];
 
-  for(var i=0; i<word.length;i++){
+  for (var i = 0; i < word.length;i++) {
 
-      if(typeof(word[i])==="string"  && word.length>=0){
-
-         newString= word.toUpperCase();
-         positionAlphabetAscii=newString[i].charCodeAt();
-         //Posición de la letra en ASCII
-         newPositionEncript=((positionAlphabetAscii-65)+33)%26+65;
-         //Con fórmula para encontrar la nueva posición encriptada
-         encripWord.push(String.fromCharCode(newPositionEncript));
-         //Array que almacena la nueva encriptación
-      }
+    if (typeof(word[i]) === 'string' && word.length >= 0) {
+      newString = word.toUpperCase();
+      positionAlphabetAscii = newString[i].charCodeAt();
+      // Posición de la letra en ASCII
+      newPositionEncript = ((positionAlphabetAscii - 65) + 33) % 26 + 65;
+      // Con fórmula para encontrar la nueva posición encriptada
+      encripWord.push(String.fromCharCode(newPositionEncript));
+      // Array que almacena la nueva encriptación
+    }
   }
 
   return encripWord;
 }
-console.log(cipher("ABCD"));//devuelve [ 'H', 'I', 'J', 'K' ]
+console.log(cipher('ABCD'));// devuelve [ 'H', 'I', 'J', 'K' ]
 
-function decipher(word2){
+function decipher(word2) {
   var positionEncript;
   var newPositionNoEncript;
-  var newArrayNoEncript=[];
-    for(var i =0; i<word2.length;i++){
-      positionEncript=word2[i].charCodeAt();
-      //Posición de la letra en ASCII
-      newPositionNoEncript=((positionAlphabetAscii-65)+33)%26;
-      //Con fórmula para encontrar la posición no encriptada
-      newArrayNoEncript.push(String.fromCharCode(newPositionNoEncript));
-      //Array que almacena la nueva encriptación
-    }
+  var newArrayNoEncript = [];
+  for (var i =0; i<word2.length;i++) {
+    positionEncript = word2[i].charCodeAt();
+    // Posición de la letra en ASCII
+    newPositionNoEncript = ((positionAlphabetAscii - 65) + 33) % 26;
+    // Con fórmula para encontrar la posición no encriptada
+    newArrayNoEncript.push(String.fromCharCode(newPositionNoEncript));
+    // Array que almacena la nueva encriptación
+  }
   return newPositionNoEncript;
 }
-decipher("A");
+decipher('A');
