6165e30945450f233bae6cb7555c89a73bf95cff
Claudiagari <claudia.garibotto@gmail.com>
2017-11-07 16:37:38 -0500

    corrigiendo errores

--- js/app.js
+++ js/app.js
@@ -6,18 +6,14 @@ function cipher(text, number) {
   // recorriendo el parametro text
   for (var i = 0;i < text.length;i++) {
   // variable que contendra los numeros ASCII
-    var k = text.charCodeAt(i);
-    // dando la condicion si la variable k esta en el rango de mayusculas
-    if (65 <= k && k <= 90) {
-      result += String.fromCharCode(((k - 65 + number) % 26) + 65);
-    }
-    // Dando la condicion la variable k pertenece a minusculas
-    else if (97 <= k && k <= 122) {
+    var letter = text.charCodeAt(i);
+    // dando la condicion si la variable letter esta en el rango de mayusculas
+    if (65 <= letter && letter <= 90) {
+      result += String.fromCharCode(((letter - 65 + number) % 26) + 65);
+    } else if (97 <= letter && letter <= 122) {
     // aplicando la formula que devuelva la letra encriptada en minusculas
-      result += String.fromCharCode(((k - 97 + number) % 26) + 97);
-    }
-    // si no es una letra devuelve una alerta
-    else {
+      result += String.fromCharCode(((letter - 97 + number) % 26) + 97);
+    } else {
       result = alert('Ingresar solo letras');
     }
   }
@@ -36,21 +32,15 @@ function decipher(text, number) {
   // recorriendo las posiciones de text
   for (var i = 0;i < text.length;i++) {
   // recorriendo las posiciones de text
-    var k = text.charCodeAt(i);
+    var letter = text.charCodeAt(i);
     // creando la condicion de mayusculas
-    if (65 <= k && k <= 90) {
+    if (65 <= letter && letter <= 90) {
     // aplicando la formula para devolver letra original en mayusculas
-      result += String.fromCharCode((k - number) + 26);
-    }
-    // (72-33)%26-no da el valor esperado/(73-33)+26/(74-33)+26 probando + en vez de %- si da la letra original :)
-
-    // creando la condicion para minusculas
-    else if (97 <= k && k <= 122) {
+      result += String.fromCharCode((letter - number) + 26);
+    } else if (97 <= letter && letter <= 122) {
     // aplicando la formula para devolver la letra original en minusculas
-      result += String.fromCharCode((k - number) + 26);
-    }
-    // de lo contrario enviar una alerta
-    else {
+      result += String.fromCharCode((letter - number) + 26);
+    } else {
       result = alert('Ingresar solo letras');
     }
   }
