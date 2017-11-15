3f3f1ba0c1060d59c893db327dfe0499cf0a02f3
Gabrielagm <gabrielagomezmilla@gmail.com>
2017-11-14 22:10:16 -0500

    añade correciones: readme, diagrama de flujo, pseudocódigo, código js

--- README.md
+++ README.md
@@ -15,23 +15,52 @@ Consideraciones:
 
 ### Pseudocódigo
 
-~~~~
+        INICIO
 
-usuario ingresa datos
-lee datos
-procedimiento cipher
-para (var i=0; i<word.length;i++)
-Si datos es igual a string y no está vacío
-convertir a mayúsculas
-ubicar valor ASCCI
-Aplicar cifrado Cesar
-Almacenar en array
-fin para
-procedimiento decipher
-para (var i =0; i<word2.length;i++)
-ubicar valor ASCCI
-Aplicar cifrado Cesar
-Almacenar en array
-fin para
+        Función cipher(message)
+            newString <- message.toUpperCase()
+        Para (j = 0; j < newString.length; j++)
+        Si newString[j] >= 65 y newString <= 90 Entonces
+        positionAlphabetAscii <- newString[j].charCodeAt()
+        newPositionEncript <- aplicamos formula de cifrado de cesar a positionAlphabetAscii
+        encriptWord += newPositionEncript
+        Si no Entonces
+        encriptWord <- alert('Solo se aceptan letras. Intentalo  nuevamente!')
+        Fin Si
+        Fin Para
 
-~~~~
+        Retornar encriptWord
+
+        Fin Función
+        Función decipher(message)
+        newString <- message.toUpperCase()
+        Para (j = 0; j < newString.length; j++)
+        Si newString[j] >= 65 y newString <= 90 Entonces
+        positionAlphabetAscii <- newString[j].charCodeAt()
+        newPositionEncript <- aplicamos formula de decifrado de cesar a positionAlphabetAscii
+        encriptWord += newPositionEncript
+        Si no Entonces
+        encriptWord <- alert('Solo se aceptan letras. Intentalo  nuevamente!')
+        Fin Si
+        Fin Para
+
+        Retornar encriptWord
+
+        Fin Función
+        Hacer message <- prompt('Ingrese una palabra')
+        Si message === null
+        alert('Regresa pronto :) ')
+        Si no si message.length === 0 Entonces
+        alert('Tu mensaje esta vacio intentalo nuevamente')
+        Si  no menuOpc = '1.-Cifrar\n' + '2.-Decifrar' Entonces
+        opc <- prompt(menuOpc, 'Elige una opción:')
+        Si opc === '1' Entonces
+        Llamar Función cipher
+        Si no si opc === '2' Entonces
+        Llamar Función decipher
+        Si no Entonces
+        alert('Intentalo nuevamente')
+
+        Fin Si
+        Repetir Mientras (!message)
+        Fin
--- js/app.js
+++ js/app.js
@@ -1,39 +1,71 @@
+// Encription function
 function cipher(word) {
-
-  var newString;
+  var newString = word.toUpperCase();
   var newPositionEncript;
   var positionAlphabetAscii;
-  var encripWord = [];
+  var encripWord = '';
 
   for (var i = 0; i < word.length;i++) {
-
-    if (typeof(word[i]) === 'string' && word.length >= 0) {
-      newString = word.toUpperCase();
+    if (newString[i].charCodeAt() >= 65 && newString[i].charCodeAt() <= 90) {
       positionAlphabetAscii = newString[i].charCodeAt();
-      // Posición de la letra en ASCII
+      // ASCII letter position
       newPositionEncript = ((positionAlphabetAscii - 65) + 33) % 26 + 65;
-      // Con fórmula para encontrar la nueva posición encriptada
-      encripWord.push(String.fromCharCode(newPositionEncript));
-      // Array que almacena la nueva encriptación
+      // Finding the new encrypted position (with formula)
+      encripWord = encripWord + String.fromCharCode(newPositionEncript);
+      // concatenated from the new encryption
+    } else {
+      encripWord = alert('Only letters are accepted. Try again!');
+      break;
     }
   }
-
   return encripWord;
-}
-console.log(cipher('ABCD'));// devuelve [ 'H', 'I', 'J', 'K' ]
+} 
 
-function decipher(word2) {
-  var positionEncript;
+
+// decryption function
+function decipher(message) {
+  var newString = message.toUpperCase();
   var newPositionNoEncript;
-  var newArrayNoEncript = [];
-  for (var i =0; i<word2.length;i++) {
-    positionEncript = word2[i].charCodeAt();
-    // Posición de la letra en ASCII
-    newPositionNoEncript = ((positionAlphabetAscii - 65) + 33) % 26;
-    // Con fórmula para encontrar la posición no encriptada
-    newArrayNoEncript.push(String.fromCharCode(newPositionNoEncript));
-    // Array que almacena la nueva encriptación
+  var positionAlphabetAscii;
+  var noEncripWord = '';
+  
+  for (var j = 0; j < newString.length; j++) {
+    if (newString[j].charCodeAt() >= 65 && newString[j].charCodeAt() <= 90) {
+      positionAlphabetAscii = newString[j].charCodeAt();  
+      // ASCII letter position
+      newPositionNoEncript = ((positionAlphabetAscii - 33 + 65) % 26) + 65;  
+      // finding the new deciphered position (with formula)
+      noEncripWord += String.fromCharCode(newPositionNoEncript);
+      // concatenated from the new decryption
+    } else {
+      noEncripWord = alert('Only letters are accepted. Try again!');
+      break;
+    }
   }
-  return newPositionNoEncript;
+  return noEncripWord;
 }
-decipher('A');
+
+
+do {
+  var message = '';
+  var message = prompt('Enter a word:');
+  
+  if (message === null) {
+    alert('Come back soon :) ');
+    break;
+  } else if (message.length === 0) {
+    alert('Your message is empty, try again');
+  } else {
+    var menuOpc = '1.- Cipher\n ' + '2.- Decipher';
+    var opc = prompt(menuOpc, 'Choose an option:');
+  
+    if (opc === '1') {
+      cipher(message);
+    } else if (opc === '2') {
+      decipher(message);
+    } else {
+      alert('Try again');
+      message = '';
+    }
+  }
+} while (message.length === 0);
