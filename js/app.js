4788cb008837b15e29cb68e828afc4bd3dd0ca62
milagrospalma <mnpalma.2@gmail.com>
2017-11-16 17:52:27 -0500

    Actualiza archivo js

--- js/app.js
+++ js/app.js
@@ -1,44 +1,49 @@
-//Se ejecuta cuando se selecciona una opción
-var menu = function (option){
-    (option == 1) ? prompt('Mensaje a cifrar:'+prompt('---'+prompt('---\n' +0))) :
-    (option == 2) ? prompt('Mensaje a descifrar\n'+0) :
-    (option == 3) ? alert('Gracias por su visita.') : alert('No tenemos esa opción.');
+var phrase = prompt('Ingrese el mensaje a cifrar:');
+
+if (phrase === '') {
+  alert('Por favor, ingrese un mensaje.');
 }
-
-//Para mostrar una ventana emergente con distintas opciones
-do{
-    var strMenu = '1. Cifrar un mensaje\n2. Descrifrar un mensaje\n3. Salir';
-    var option = prompt('Seleccione una opción:\n' + strMenu);
-    menu(option);
-}while(option != 3);
-
-//Función para descifrar una frase
-var decipher = function(phrase) { // ( x - 65 + n) % 26 + 65
-
-}
-
-//Función para cifrar una frase
-var cipher = function(phrase) { // ( x + n ) % 26 -> # que debe sumar
-  var index; //posicion de letra
-  var n=33; //valor fijo
-
-  var cod = '';
-
-  for (index=0; index<phrase.length; index++) {
-    //console.log(phrase[index]);
-    //Obtenemos el número de posiciones que debe avanzar
-    var numPositions = (index+n) % 26;
-    console.log(numPositions)
-    //Sumando el índice (posición actual) y la cantidad de posiciones que debe avanzar
-    var newIndex = index+numPositions;
-    console.log(newIndex) //7 9 11 13 15 17
-    cod += phrase[newIndex];
-    console.log('imprime '+cod);
-    /*return cod;*/
+// Función para cifrar una frase
+var cipher = function(phrase) {
+  var numberFixed = 33; // valor fijo
+  if (phrase == parseInt(phrase)) {
+    alert('Ingrese solo letras.');
+  } else {
+    var arrCodeAscii = [];
+    var newPhrase = [];
+    for (var i = 0; i < phrase.length; i++) {
+      var letter = phrase.charAt(i);
+      if (letter == letter.toUpperCase()) {
+        var asciiUppercase = letter.charCodeAt(0);
+        newAsciiUppercase = (asciiUppercase - 65 + numberFixed) % 26 + 65;
+        arrCodeAscii.push(newAsciiUppercase);
+      } else {
+        var asciiLowercase = letter.charCodeAt(0);
+        newAsciiLowercase = (asciiLowercase - 97 + numberFixed) % 26 + 97;
+        arrCodeAscii.push(newAsciiLowercase);
+      }
+    }
+    for (var j = 0; j < arrCodeAscii.length; j++) {
+      var newLetter = String.fromCharCode(arrCodeAscii[j]);
+      newPhrase.push(newLetter);
+    }
   }
-}
+  if (typeof(phrase) === 'string') {
+    alert('Mensaje cifrado: ' + newPhrase.join(''));
+  }
+};
+cipher(phrase);
 
-//var original = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-//var descifrar = 'HIJKLMNOPQRSTUVWXYZABCDEFG';
+/*
+// Se ejecuta cuando se selecciona una opción
+var menu = function(option) {
+  (option == 1) ? prompt('Ingrese el mensaje a cifrar:') : (option == 2) ? prompt('Ingrese el mensaje a descifrar:') : (option == 3) ? alert('Gracias por su visita.') : alert('No tenemos esa opción.');
+};
 
-//console.log(cipher(original));
+// Para mostrar una ventana emergente con distintas opciones
+do {
+  var strMenu = '1. Cifrar un mensaje \n 2. Descrifrar un mensaje \n 3. Salir';
+  var option = prompt('Seleccione una opción:\n' + strMenu);
+  menu(option);
+} while (option != 3);
+*/
