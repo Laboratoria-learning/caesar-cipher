ce298f7915bca95387f7d9c26b4d2f631deaf86d
aurora <selva_beat@hotmail.com>
2017-11-21 16:08:24 -0500

    corrigiendo archivo js

--- js/app.js
+++ js/app.js
@@ -4,40 +4,23 @@ convertNumbers = parseInt(words) ;
 
 console.log(convertNumbers) ;
 
+if (!isNaN(convertNumbers) || words === '') {
+  alert('error');
+} else {
+  var noIsNumber = words ;
+}
 
-  if ( !isNaN(convertNumbers) || words === '' ) {
+var cipher = function(wordX) {
 
-      alert('error');
+  var wordUppercase = wordX.toUpperCase();
+  var arr = wordUppercase ;
 
+  for (var i = 0 ; i < arr.length ; i++) {
+    var arrCode = arr.charCodeAt(i) ;
+    var arrNewCode = (arrCode - 65 + 33) % 26 + 65 ;
+    var newStringCode = String.fromCharCode(arrNewCode) ;
   }
+  return newStringCode ;
+};
 
-  else {
-
-      var noIsNumber = words ;
-
-  }
-
-
-  var cipher = function(wordX) {
-
-      var wordUppercase = wordX.toUpperCase();
-
-      var arr = wordUppercase ;
-
-
-   for (var i=0 ; i<arr.length ; i++) {
-
-     var  arrCharcodeAt = arr.charCodeAt(i) ;
-
-     var arrCaesarCipher = ( arrCharcodeAt-65+33 ) % 26 + 65 ;
-
-     var newStringCode = String.fromCharCode(arrCaesarCipher) ;
-
-   }
-
-   return newStringCode ;
-
- }
-
-
- document.write( cipher(noIsNumber) );
+document.write(cipher(noIsNumber));
