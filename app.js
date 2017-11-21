b42d002ea31473c7c3ec4cb7f4eda717884b66e2
alexandracch <alexa_theprinces@hotmail.com>
2017-11-21 10:54:41 -0500

    siguiendo guía de estilos

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
 var strMenu = '1. Encriptar\n2. Desencriptar\n';
-var menu =  prompt ('¿Qué desea hacer?: \n'+ strMenu);
-var phrase = prompt('Inserta una frase a encriptar')
+var menu = prompt('¿Qué desea hacer?: \n' + strMenu);
+var phrase = prompt('Inserta una frase a encriptar');
 
-function cipher(phrase,base){
-  var codeAscii= [];
+function cipher(phrase,base) {
+  var codeAscii = [];
 
-  for (i = 0; i < phrase.length; i++){
-
+  for (i = 0; i < phrase.length; i++) {
     var letter = phrase.charAt(i);
 
     if (letter === letter.toUpperCase()) {
 
       var letterUp = letter.charCodeAt(0);
 
-      letterUp = (letterUp-65+base)%26+65;
+      letterUp = (letterUp - 65 + base) % 26 + 65;
 
       codeAscii.push(letterUp);
-
-    }
-    else{
-      var letterDown = (letterDown - 97 + base)%26+97;
-
-     codeAscii.push(letterDown);
-
+    } else {
+      var letterDown = (letterDown - 97 + base) % 26 + 97;
+      codeAscii.push(letterDown);
     }
   }
 
@@ -30,10 +25,11 @@ function cipher(phrase,base){
 
   var newPhrase = ' ';
 
-  for (e = 0; e< newArray.length;e++);
-  var newLetter = String.fromCharCode(newArray[e]);
-  var newPhrase = newLetter.concat(newPhrase);
+  for (e = 0; e < newArray.length; e++) {
+    var newLetter = String.fromCharCode(newArray[e]);
+    var newPhrase = newLetter.concat(newPhrase);
 
-  return newPhrase;
+    return newPhrase;
+  }
 }
- console.log(cipher(phrase,33));
+console.log(cipher(phrase, 33));
